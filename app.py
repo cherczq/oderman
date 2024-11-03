9b6846efbbbdf92c3050399322ec656d8cd72042
cherczq <sherkashinaanna11@gmail.com>
2024-11-03 21:36:02 +0200

    app.py

--- app.py
+++ app.py
@@ -22,12 +22,27 @@ def post_login():
     info = request.user_agent
     if user == "admin":
         return f"Are you is {user} from {info}"
- 
+    else:
+        return redirect(url_for("get_login"), code=302)
 
-
-@app.get("/menu/")
-def get_menu():
-    return render_template("menu.html")
+max_price = 50
+name = "Oderman-menu"
+menu = [
+  {"name": "Hawaiian Pizza",  "composition": "ham,pineapple,tomato sauce,ham","price":30},
+  {"name": "Pepperoni Pizza","composition": "Tomato sauce, mozzarella cheese, and pepperoni", "price":40},
+  {"name": "Margherita Pizza", "composition": "Fresh mozzarella, tomato sauce,and basil","price":17},
+  {"name": "Meat Lovers Pizza", "composition": "Tomato sauce, mozzarella cheese, pepperoni, sausage, bacon, and ham","price": 26},
+  {"name": "White Pizza",  "composition": "Mozzarella cheese, ricotta cheese, garlic, and spinach.","price": 34},
+]
+@app.get('/menu/')
+def results():
+  context={
+     "title": "Oderman",
+     "Pizzas": menu,
+     "pizzas_name": name,
+     "max_prise": max_price,
+  }
+  return  render_template("menu.html",**context)
 
 
 if __name__ == '__main__':
